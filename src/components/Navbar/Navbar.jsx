5354d5e83fdc769679feae91aa3f0e100093b7f3
Akash Tiwary <tiwaryakash0308@gmail.com>
2023-01-06 18:19:23 +0530

    comment

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css';
+import logo from '../../assets/logo-3.png'
+import SearchIcon from '@mui/icons-material/Search';
+
+// wskdnjsnd
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <div className='left'>
+                <div className='navlogo'>
+                    <img src={logo} alt='Not loaded'/>
+                </div>
+                <div className="nav_Searchbar">
+                  <input type= "text" name="" id="" />
+                  <div className="search_icon">
+                    <SearchIcon id='search`' />
+                  </div>
+                </div>
+            </div>
+            <div className='right'></div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
