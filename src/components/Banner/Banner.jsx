1b99ed85b8ea2d7c2d408ad3e0876685a978c55f
Adarsh <adarsh800251@gmail.com>
2023-01-13 19:27:01 +0530

    Banner added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Carousel from 'react-material-ui-carousel'
+import { bannerImages } from '../../helper/dummy_data'
+import './banner.css'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <>
+     <Carousel
+     className='carasousel'
+     autoPlay={true}
+     animation='slide'
+     indicators={false}
+     navButtonsAlwaysVisible={true}
+     cycleNavigation={true}
+     navButtonsProps={{
+      style:{
+        background:'rgba(255, 255, 255,0.9)',
+        color:'black'
+      }
+     }}
+     >
+      {bannerImages.map((pic,i)=>{
+        return(
+          <img key={i} src={pic} alt='img' className='banner_img'/>
+        ) 
+      })}
+     </Carousel>
+    </>
   )
 }
 
