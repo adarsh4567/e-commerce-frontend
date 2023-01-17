215a1aa5fd72b3f83d1fa0083a63672bd20c76b7
Akash Tiwary <tiwaryakash0308@gmail.com>
2023-01-17 20:53:25 +0530

    slide component added

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
 import './mainComponent.css'
+import Slide from '../slide/slide'
 
 const MainComponent = () => {
   return (
@@ -9,6 +10,7 @@ const MainComponent = () => {
         <div className='banner_part'>
            <Banner/>
         </div>
+        <Slide/>
     </div>
     </>
   )
