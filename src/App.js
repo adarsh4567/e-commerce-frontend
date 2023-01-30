292df8dfa61aa3acb1657dca1b670854b39c62a4
Akash Tiwary <tiwaryakash0308@gmail.com>
2023-01-30 19:36:27 +0530

    Merge branch 'test-2' of https://github.com/adarsh4567/e-commerce into test-1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SubNav from './components/subNav/SubNav';
 import Footer from './components/Footer/Footer';
 import { Routes,Route } from 'react-router-dom';
 import Auth from './components/Auth/Auth';
+import Cart from './components/Cart/Cart';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
     <Routes>
       <Route path='/' element={<MainComponent/>}/>
       <Route path='/login' element={<Auth/>}/>
+      <Route path='/getproductone/id' element={<Cart/>}/>
     </Routes>
     <Footer />
     </>);
