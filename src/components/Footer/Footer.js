7d2d09898ea027c6684efb753289be9b6085bcb3
Adarsh <adarsh800251@gmail.com>
2023-02-02 16:51:21 +0530

    footer description updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -39,7 +39,7 @@ const Footer = () => {
       </div>
       <div className="lastdetails">
                <p>eTown</p>
-                <p>Conditions of Use & Sale &nbsp; &nbsp;&nbsp;  Privacy Notice  &nbsp; &nbsp;&nbsp; Interest-Based Ads  &nbsp; &nbsp;&nbsp;  © 1996-{year}, Amazon.com, Inc. or its affiliates</p>
+                <p>Conditions of Use & Sale &nbsp; &nbsp;&nbsp;  Privacy Notice  &nbsp; &nbsp;&nbsp; Interest-Based Ads  &nbsp; &nbsp;&nbsp;  © 1996-{year}, e-town.vercel.app, Inc. or its affiliates</p>
             </div>
     </footer>
   )
