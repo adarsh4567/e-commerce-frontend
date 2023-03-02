13e6aa2347324a972e055e7968dd110691f3b98d
Adarsh <adarsh800251@gmail.com>
2023-03-02 13:00:39 +0530

    added banner images/product images

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { QueryClient,QueryClientProvider } from 'react-query';
+
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <Provider store={store}>
   <BrowserRouter>
+  <QueryClientProvider client={queryClient}>
     <App />
+  </QueryClientProvider>
   </BrowserRouter>
   </Provider>
 );
